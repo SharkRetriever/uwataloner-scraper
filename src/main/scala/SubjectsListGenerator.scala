845d5efa569315e42f58927e89a521ffe84f9f32
sharkretriever <thermostatico@gmail.com>
2018-05-29 10:52:17 -0400

    Only adding UW subjects offered this term

--- src/main/scala/SubjectsListGenerator.scala
+++ src/main/scala/SubjectsListGenerator.scala
@@ -3,16 +3,50 @@ import java.net.URL
 
 import com.eclipsesource.json.{Json, JsonArray, JsonValue}
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable
+import scala.collection.mutable.{ListBuffer, Set}
 
 object SubjectsListGenerator {
   def generateSubjectsListCode(): String = {
     try {
-      val subjectsList: ListBuffer[(String, String)] = new ListBuffer[(String, String)]
+      // We first grab the list of courses offered this term
+      val termSubjects: mutable.Set[String] = mutable.Set()
 
+      val termUrl: URL = new URL(s"https://api.uwaterloo.ca/v2/terms/list.json?key=${KeyHolder.Key}")
+      val termJson: JsonValue = Json.parse(new InputStreamReader(termUrl.openStream()))
+      var status: Int = termJson.asObject().get("meta").asObject().getInt("status", -1)
+      if (status != 200) {
+        throw new InvalidObjectException("Invalid status")
+      }
+
+      val termId: Int = termJson.asObject().get("data").asObject().getInt("current_term", -1)
+      if (termId < 0) {
+        throw new InvalidObjectException("Invalid term")
+      }
+
+      val coursesListUrl: URL = new URL(s"https://api.uwaterloo.ca/v2/terms/$termId/courses.json?key=${KeyHolder.Key}")
+      val coursesListJson: JsonValue = Json.parse(new InputStreamReader(coursesListUrl.openStream()))
+      status = coursesListJson.asObject().get("meta").asObject().getInt("status", -1)
+      if (status != 200) {
+        throw new InvalidObjectException("Invalid status")
+      }
+
+      val coursesArray: JsonArray = coursesListJson.asObject().get("data").asArray()
+
+      for (i <- 0 until coursesArray.size()) {
+        val currentCourse: JsonValue = coursesArray.get(i)
+        val subjectName: String = currentCourse.asObject().getString("subject", "")
+        if (subjectName.isEmpty) {
+          throw new InvalidObjectException("Invalid subject name detected")
+        }
+        termSubjects += subjectName
+      }
+
+      // now we have the distinct list of subjects offered this term, so start grabbing descriptions
+      val subjectsList: ListBuffer[(String, String)] = new ListBuffer[(String, String)]
       val subjectsListUrl: URL = new URL(s"https://api.uwaterloo.ca/v2/codes/subjects.json?key=${KeyHolder.Key}")
       val subjectsListJson: JsonValue = Json.parse(new InputStreamReader(subjectsListUrl.openStream()))
-      val status: Int = subjectsListJson.asObject().get("meta").asObject().getInt("status", -1)
+      status = subjectsListJson.asObject().get("meta").asObject().getInt("status", -1)
       if (status != 200) {
         throw new InvalidObjectException("Invalid status")
       }
@@ -25,7 +59,9 @@ object SubjectsListGenerator {
           if (subjectName.isEmpty || subjectDescription.isEmpty) {
             throw new InvalidObjectException("Invalid subject name-description pair detected")
           }
-          subjectsList += ((subjectName, subjectDescription))
+          if (termSubjects.contains(subjectName) && !subjectDescription.contains("(WLU)")) {
+            subjectsList += ((subjectName, subjectDescription))
+          }
         }
 
         val header = "fun getSubjectsList(): List<Subject> {\r\n\treturn listOf("
